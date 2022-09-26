5d24df0755754a268dc72f551d4f0b0e9de1215f
Eden Federman <eden@keyval.dev>
2022-09-26 11:31:16 +0300

    add map space

--- payload/mmap.asm
+++ payload/mmap.asm
@@ -2,7 +2,7 @@
 mov r8, -1            ; annonymous fd
 mov rax, 9            ; mmap number
 mov rdi, 0            ; operating system will choose mapping destination
-mov rsi, 12288        ; page size
+mov rsi, 12582912     ; page size
 mov rdx, 0x7          ; PROT_READ|PROT_WRITE|PROT_EXEC
 mov r10, 0x8022       ; MAP_PRIVATE|MAP_ANON|MAP_POPULATE             
 mov r9, 0             ; offset inside test.txt
